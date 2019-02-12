f2002b21a1a245323a8eb4fe8706e0db366a71c6
Simon Garcia De Gonzalo <grcdgnz2@illinois.edu>
2019-02-12 16:20:10 -0600

    deteled cmake and make generated files, should be good to go now

--- labs/bfs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/simon/ECE508_template/gpu-algorithms-labs/labs/bfs
-# Build directory: /home/simon/ECE508_template/gpu-algorithms-labs/labs/bfs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
