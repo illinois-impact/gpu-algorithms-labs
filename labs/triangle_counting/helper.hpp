86178bb28fad0f557f919af786b753382fbcf239
Carl Pearson <carl.w.pearson@gmail.com>
2019-03-01 17:00:45 -0600

    wei's changes

--- labs/triangle_counting/helper.hpp
+++ labs/triangle_counting/helper.hpp
@@ -22,51 +22,6 @@
 
 #include <cuda.h>
 
-void compute(unsigned int numNodes, unsigned int *nodePtrs,
-             unsigned int *nodeNeighbors, unsigned int *nodeVisited,
-             unsigned int *nodeVisited_ref, unsigned int *currLevelNodes,
-             unsigned int *nextLevelNodes_ref, unsigned int *numCurrLevelNodes,
-             unsigned int *numNextLevelNodes_ref) {
-
-  // Compute reference out
-  // Loop over all nodes in the curent level
-  for (unsigned int idx = 0; idx < *numCurrLevelNodes; ++idx) {
-    unsigned int node = currLevelNodes[idx];
-    // Loop over all neighbors of the node
-    for (unsigned int nbrIdx = nodePtrs[node]; nbrIdx < nodePtrs[node + 1];
-         ++nbrIdx) {
-      unsigned int neighbor = nodeNeighbors[nbrIdx];
-      // If the neighbor hasn't been visited yet
-      if (!nodeVisited_ref[neighbor]) {
-        // Mark it and add it to the queue
-        nodeVisited_ref[neighbor] = 1;
-        nextLevelNodes_ref[*numNextLevelNodes_ref] = neighbor;
-        ++*numNextLevelNodes_ref;
-      }
-    }
-  }
-}
-
-static bool verify(unsigned int *numNextLevelNodes_ref,
-                   unsigned int *NextLevelNodes_ref,
-                   unsigned int *numNextLevelNodes_solution,
-                   unsigned int *NextLevelNodes_solution) {
-
-  INFO("Frontier size did not match");
-  REQUIRE(*numNextLevelNodes_ref == *numNextLevelNodes_solution);
-
-  std::sort(NextLevelNodes_solution,
-            NextLevelNodes_solution + (*numNextLevelNodes_solution));
-  std::sort(NextLevelNodes_ref, NextLevelNodes_ref + (*numNextLevelNodes_ref));
-
-  for (int i = 0; i < *numNextLevelNodes_ref; i++) {
-
-    INFO("Sorted frontier did not match solution");
-    REQUIRE(NextLevelNodes_ref[i] == NextLevelNodes_solution[i]);
-  }
-  return true;
-}
-
 static std::chrono::time_point<std::chrono::high_resolution_clock> now() {
   return std::chrono::high_resolution_clock::now();
 }
@@ -80,64 +35,6 @@ void checkCuda(cudaError_t result, const char *file, const int line) {
     exit(-1);
   }
 }
-
-void setupProblem(unsigned int numNodes, unsigned int maxNeighborsPerNode,
-                  unsigned int **nodePtrs_h, unsigned int **nodeNeighbors_h,
-                  unsigned int **totalNeighbors_h, unsigned int **nodeVisited_h,
-                  unsigned int **nodeVisited_ref,
-                  unsigned int **currLevelNodes_h,
-                  unsigned int **nextLevelNodes_h,
-                  unsigned int **nextLevelNodes_ref,
-                  unsigned int **numCurrLevelNodes_h,
-                  unsigned int **numNextLevelNodes_h,
-                  unsigned int **numNextLevelNodes_ref) {
-
-  // Initialize node pointers
-  *nodePtrs_h = (unsigned int *)malloc((numNodes + 1) * sizeof(unsigned int));
-  *nodeVisited_h = (unsigned int *)malloc(numNodes * sizeof(unsigned int));
-  *nodeVisited_ref = (unsigned int *)malloc(numNodes * sizeof(unsigned int));
-  (*nodePtrs_h)[0] = 0;
-  for (unsigned int node = 0; node < numNodes; ++node) {
-    const unsigned int numNeighbors = rand() % (maxNeighborsPerNode + 1);
-    (*nodePtrs_h)[node + 1] = (*nodePtrs_h)[node] + numNeighbors;
-    (*nodeVisited_h)[node] = (*nodeVisited_ref)[node] = 0;
-  }
-
-  // Initialize neighbors
-  *totalNeighbors_h = (unsigned int *)malloc(sizeof(unsigned int));
-  **totalNeighbors_h = (*nodePtrs_h)[numNodes];
-  *nodeNeighbors_h =
-      (unsigned int *)malloc((**totalNeighbors_h) * sizeof(unsigned int));
-  for (unsigned int neighborIdx = 0; neighborIdx < **totalNeighbors_h;
-       ++neighborIdx) {
-    (*nodeNeighbors_h)[neighborIdx] = rand() % numNodes;
-  }
-
-  // Initialize current level
-  *numCurrLevelNodes_h = (unsigned int *)malloc(sizeof(unsigned int));
-  **numCurrLevelNodes_h = numNodes / 10; // Let level contain 10% of all nodes
-  *currLevelNodes_h =
-      (unsigned int *)malloc((**numCurrLevelNodes_h) * sizeof(unsigned int));
-  for (unsigned int idx = 0; idx < **numCurrLevelNodes_h; ++idx) {
-    // Find a node that's not visited yet
-    unsigned node;
-    do {
-      node = rand() % numNodes;
-    } while ((*nodeVisited_h)[node]);
-    (*currLevelNodes_h)[idx] = node;
-    (*nodeVisited_h)[node] = (*nodeVisited_ref)[node] = 1;
-  }
-
-  // Prepare next level containers (i.e. output variables)
-  *numNextLevelNodes_h = (unsigned int *)malloc(sizeof(unsigned int));
-  **numNextLevelNodes_h = 0;
-  *numNextLevelNodes_ref = (unsigned int *)malloc(sizeof(unsigned int));
-  **numNextLevelNodes_ref = 0;
-  *nextLevelNodes_h = (unsigned int *)malloc((numNodes) * sizeof(unsigned int));
-  *nextLevelNodes_ref =
-      (unsigned int *)malloc((numNodes) * sizeof(unsigned int));
-}
-
 
 inline bool endsWith (const std::string &fullString, const std::string &ending) {
   if (fullString.length() >= ending.length()) {
--- labs/triangle_counting/template.cu
+++ labs/triangle_counting/template.cu
@@ -2,31 +2,50 @@
 #include <cstdlib>
 #include <stdio.h>
 
-#include "solution.hu"
+#include "template.hu"
 
+__global__ static void kernel_tc(uint64_t *__restrict__ triangleCounts, //!< per-edge triangle counts
+                                 const uint32_t *const edgeSrc,         //!< node ids for edge srcs
+                                 const uint32_t *const edgeDst,         //!< node ids for edge dsts
+                                 const uint32_t *const rowPtr,          //!< source node offsets in edgeDst
+                                 const size_t numEdges                  //!< how many edges to count triangles for
+) {
+
+  // Determine the source and destination node for the edge
+
+  // Use the row pointer array to determine the start and end of the neighbor list in the column index array
+
+  // Determine how many elements of those two arrays are common
+}
 
 uint64_t count_triangles(const pangolin::COOView<uint32_t> view, const int mode) {
   if (mode == 1) {
 
     // REQUIRED
-    
 
-    //@@ create a pangolin::Vector to hold per-edge triangle counts
+    //@@ create a pangolin::Vector (uint64_t) to hold per-edge triangle counts
+    // Pangolin is backed by CUDA so you do not need to explicitly copy data between host and device.
+    // You may find pangolin::Vector::data() function useful to get a pointer for your kernel to use.
 
     //@@ launch the linear search kernel here
+    dim3 dimBlock(512);
+    // dim3 dimGrid (ceil(number of non-zeros / dimBlock.x))
+    // kernel_tc<<<dimGrid, dimBlock>>>(...)
 
-    //@@ do a global reduction to produce the final triangle count
     uint64_t total = 0;
+    //@@ do a global reduction (on CPU or GPU) to produce the final triangle count
+
     return total;
 
   } else if (2 == mode) {
 
-    // OPTIONAL
-    
+    // OPTIONAL. See README for more details
+
     uint64_t total = 0;
+    //@@ do a global reduction (on CPU or GPU) to produce the final triangle count
+
     return total;
-  }
-  else {
+  } else {
     assert("Unexpected mode");
     return uint64_t(-1);
   }
